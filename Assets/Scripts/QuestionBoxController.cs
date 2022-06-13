e436a04c151d4ff961015b57232a7b2338edf8ae
ongkahyuan <ongkahyuan@gmail.com>
2022-06-13 20:32:21 +0800

    Finish part 1
    Done with audio things under "Unity for Children" (still need to add custom sounds)

--- Assets/Scripts/QuestionBoxController.cs
+++ Assets/Scripts/QuestionBoxController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class QuestionBoxController : MonoBehaviour
 {
     public Rigidbody2D rigidBody;
+    public AudioSource bumpAudio;
     public SpringJoint2D springJoint;
     public GameObject consummablePrefab;//mushroom prefab
     public SpriteRenderer spriteRenderer;
@@ -13,7 +14,7 @@ public class QuestionBoxController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        bumpAudio = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -24,22 +25,28 @@ public class QuestionBoxController : MonoBehaviour
         // }
     }
 
-    void OnCollisionEnter2D(Collision2D col){
-        if(col.gameObject.CompareTag("Player") && !hit){
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player") && !hit)
+        {
             hit = true;
+            bumpAudio.PlayOneShot(bumpAudio.clip);
             // ensure we move the object sufficiently
-            rigidBody.AddForce(new Vector2(0, rigidBody.mass*20), ForceMode2D.Impulse);
+            rigidBody.AddForce(new Vector2(0, rigidBody.mass * 20), ForceMode2D.Impulse);
             // spawn the mushroom prefab slightly above the box
             Instantiate(consummablePrefab, new Vector3(this.transform.position.x, this.transform.position.y + 1.0f, this.transform.position.z), Quaternion.identity);
             StartCoroutine(DisableHittable());
         }
     }
 
-    bool ObjectMovedAndStopped(){
-        return Mathf.Abs(rigidBody.velocity.magnitude)<0.01;
+    bool ObjectMovedAndStopped()
+    {
+        return Mathf.Abs(rigidBody.velocity.magnitude) < 0.01;
     }
-    IEnumerator DisableHittable(){
-        if(!ObjectMovedAndStopped()){
+    IEnumerator DisableHittable()
+    {
+        if (!ObjectMovedAndStopped())
+        {
             yield return new WaitUntil(() => ObjectMovedAndStopped());
         }
 
